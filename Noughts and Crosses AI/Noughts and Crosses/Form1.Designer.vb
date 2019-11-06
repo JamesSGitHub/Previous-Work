d0928c206517edc70d65b72429260f1fba022779
JamesSGitHub <57457027+JamesSGitHub@users.noreply.github.com>
2019-11-06 19:21:04 +0000

    Noughts and Crosses against smart computer
    Developed in Visual Basic in 2017

---
+++ Noughts and Crosses AI/Noughts and Crosses/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe Print", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(18, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(413, 64)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Noughts and Crosses"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(8, 269)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(245, 18)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Welcome to Noughts and Crosses"
+        '
+        'Button10
+        '
+        Me.Button10.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button10.Location = New System.Drawing.Point(269, 74)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(158, 46)
+        Me.Button10.TabIndex = 11
+        Me.Button10.Text = "Start Game"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(269, 145)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(158, 121)
+        Me.ListBox1.TabIndex = 12
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.Button9)
+        Me.Panel1.Controls.Add(Me.Button8)
+        Me.Panel1.Controls.Add(Me.Button7)
+        Me.Panel1.Controls.Add(Me.Button6)
+        Me.Panel1.Controls.Add(Me.Button5)
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Location = New System.Drawing.Point(29, 74)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(191, 192)
+        Me.Panel1.TabIndex = 13
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button9.Location = New System.Drawing.Point(132, 133)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(60, 60)
+        Me.Button9.TabIndex = 18
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(66, 133)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(60, 60)
+        Me.Button8.TabIndex = 17
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(-1, 133)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(60, 60)
+        Me.Button7.TabIndex = 16
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(132, 66)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(60, 60)
+        Me.Button6.TabIndex = 15
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(66, 66)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(60, 60)
+        Me.Button5.TabIndex = 14
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(-1, 66)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(60, 60)
+        Me.Button4.TabIndex = 13
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(132, -1)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(60, 60)
+        Me.Button3.TabIndex = 12
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(66, -1)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(60, 60)
+        Me.Button2.TabIndex = 11
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Arial", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(-1, -1)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(60, 60)
+        Me.Button1.TabIndex = 10
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(443, 319)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Noughts and Crosses"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button10 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Noughts and Crosses AI/Noughts and Crosses/Form1.vb
@@ -0,0 +1,250 @@
+﻿Public Class Form1
+    Private the_board(9) As String
+    Dim the_board_position() As String = {"top left", "top centre", "top right", "mid left", "centre", "mid right", "bottom left", "bottom centre", "bottom right"}
+
+    Private Const PLAYER_SYMBOL As String = "X"
+    Private Const COMPUTER_SYMBOL As String = "O"
+    Private Const BLANK_SYMBOL As String = ""
+    Private player_turn As Boolean
+    Private game_over As Boolean = False
+
+    Function check_win(Symbol As String) As Boolean
+        Dim win As Boolean = False
+        If the_board(0) = Symbol And the_board(1) = Symbol And the_board(2) = Symbol Then
+            win = True
+        ElseIf the_board(3) = Symbol And the_board(4) = Symbol And the_board(5) = Symbol Then
+            win = True
+        ElseIf the_board(6) = Symbol And the_board(7) = Symbol And the_board(8) = Symbol Then
+            win = True
+        ElseIf the_board(0) = Symbol And the_board(3) = Symbol And the_board(6) = Symbol Then
+            win = True
+        ElseIf the_board(1) = Symbol And the_board(4) = Symbol And the_board(7) = Symbol Then
+            win = True
+        ElseIf the_board(2) = Symbol And the_board(5) = Symbol And the_board(8) = Symbol Then
+            win = True
+        ElseIf the_board(0) = Symbol And the_board(4) = Symbol And the_board(8) = Symbol Then
+            win = True
+        ElseIf the_board(2) = Symbol And the_board(4) = Symbol And the_board(6) = Symbol Then
+            win = True
+        End If
+        Return win
+    End Function
+
+    Private Function check_draw() As Boolean
+        Dim complete_board As Boolean = True
+        For i As Integer = 0 To 8
+            If the_board(i) = BLANK_SYMBOL Then
+                complete_board = False
+            End If
+        Next i
+        Return complete_board
+    End Function
+
+    Private Sub player_move(n)
+        If Me.the_board(n) = BLANK_SYMBOL Then
+            If Not game_over Then
+                Me.the_board(n) = PLAYER_SYMBOL
+                ListBox1.Items.Add("Player moves " & the_board_position(n))
+                If Not check_win(PLAYER_SYMBOL) Then
+                    game_over = check_draw()
+                    If Not game_over Then
+                        computer_move()
+                    Else
+                        display_message("It's a draw!")
+                    End If
+                Else
+                    display_message(PLAYER_SYMBOL & " win")
+                End If
+                update_board()
+            End If
+        Else
+            display_message("That move is invalid!")
+        End If
+    End Sub
+
+    Private Sub computer_move()
+        Dim move_made As Boolean = False
+        'Check whether computer has a winning move
+        move_made = winning_move(COMPUTER_SYMBOL)
+
+        'Check whether computer can block a winning move
+        If Not move_made Then
+            move_made = winning_move(PLAYER_SYMBOL)
+        End If
+
+        'Take centre if avaliable
+        If Not move_made Then
+            If the_board(4) = BLANK_SYMBOL Then
+                the_board(4) = COMPUTER_SYMBOL
+                move_made = True
+                ListBox1.Items.Add("Computer moves " & the_board_position(4))
+            End If
+        End If
+
+        'Take corner if avaliable
+        If Not move_made Then
+            move_made = take_corner()
+        End If
+
+        'Take non corner (other)
+        If Not move_made Then
+            move_made = take_other()
+        End If
+
+        If check_win(COMPUTER_SYMBOL) Then
+            game_over = True
+            display_message(COMPUTER_SYMBOL & " wins!")
+        End If
+        If check_draw() Then
+            game_over = True
+            display_message("It's a draw!")
+        End If
+
+    End Sub
+    Private Function take_corner() As Boolean
+        Dim corner_found As Boolean = False
+        If the_board(0) = PLAYER_SYMBOL And the_board(8) = PLAYER_SYMBOL Or the_board(2) = PLAYER_SYMBOL And the_board(6) = PLAYER_SYMBOL Then
+            For i As Integer = 0 To 8
+                Select Case i
+                    Case 1, 3, 5, 7
+                        If the_board(i) = BLANK_SYMBOL Then
+                            the_board(i) = COMPUTER_SYMBOL
+                            corner_found = True
+                            ListBox1.Items.Add("Computer moves " & the_board_position(i))
+                            Exit For
+                        End If
+                    Case Else
+                End Select
+            Next i
+        ElseIf the_board(5) = PLAYER_SYMBOL And the_board(7) = PLAYER_SYMBOL Then
+            For i As Integer = 0 To 8
+                Select Case i
+                    Case 8
+                        If the_board(i) = BLANK_SYMBOL Then
+                            the_board(i) = COMPUTER_SYMBOL
+                            corner_found = True
+                            ListBox1.Items.Add("Computer moves " & the_board_position(i))
+                            Exit For
+                        End If
+                    Case Else
+                End Select
+            Next i
+        Else
+            For i As Integer = 0 To 8
+                Select Case i
+                    Case 0, 2, 6, 8
+                        If the_board(i) = BLANK_SYMBOL Then
+                            the_board(i) = COMPUTER_SYMBOL
+                            corner_found = True
+                            ListBox1.Items.Add("Computer moves " & the_board_position(i))
+                            Exit For
+                        End If
+                    Case Else
+                End Select
+            Next i
+        End If
+        Return corner_found
+    End Function
+    Private Function take_other() As Boolean
+        Dim free_space_found As Boolean = False
+        For i As Integer = 0 To 8
+            Select Case i
+                Case 1, 3, 5, 7
+                    If the_board(i) = BLANK_SYMBOL Then
+                        the_board(i) = COMPUTER_SYMBOL
+                        free_space_found = True
+                        ListBox1.Items.Add("Computer moves " & the_board_position(i))
+                        Exit For
+                    End If
+                Case Else
+            End Select
+        Next i
+        Return free_space_found
+    End Function
+    Private Function winning_move(SYMBOL) As Boolean
+        Dim result As Boolean = False
+        For i As Integer = 0 To 8
+            If the_board(i) = BLANK_SYMBOL Then
+                the_board(i) = SYMBOL
+                If check_win(SYMBOL) = True Then
+                    the_board(i) = COMPUTER_SYMBOL
+                    result = True
+                    ListBox1.Items.Add("Computer moves " & the_board_position(i))
+                    Exit For
+                Else
+                    the_board(i) = BLANK_SYMBOL
+                End If
+            End If
+        Next i
+        Return result
+    End Function
+
+    Private Sub display_message(message As String)
+        Me.Label2.Text = message
+    End Sub
+
+    Private Sub initialise_game()
+        ListBox1.Items.Clear()
+        Me.game_over = False
+        For i As Integer = 0 To 8
+            Me.the_board(i) = BLANK_SYMBOL
+        Next i
+        Dim rand As Single
+        Randomize()
+        rand = Rnd()
+        If rand >= 0.5 Then
+            Me.player_turn = True
+        Else
+            Me.player_turn = False
+            computer_move()
+            Me.player_turn = True
+        End If
+        display_message("Players turn")
+    End Sub
+
+    Private Sub update_board()
+        Me.Button1.Text = the_board(0)
+        Me.Button2.Text = the_board(1)
+        Me.Button3.Text = the_board(2)
+        Me.Button4.Text = the_board(3)
+        Me.Button5.Text = the_board(4)
+        Me.Button6.Text = the_board(5)
+        Me.Button7.Text = the_board(6)
+        Me.Button8.Text = the_board(7)
+        Me.Button9.Text = the_board(8)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        player_move(0)
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        player_move(1)
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        player_move(2)
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        player_move(3)
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        player_move(4)
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        player_move(5)
+    End Sub
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        player_move(6)
+    End Sub
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        player_move(7)
+    End Sub
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        player_move(8)
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        initialise_game()
+        update_board()
+    End Sub
+End Class
+
